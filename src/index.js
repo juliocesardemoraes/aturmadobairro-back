9c9568cd3883aebfab1786ca8141a216399a68d9
juliocesardemoraes <jcmcf2012@gmail.com>
2024-05-02 11:41:51 -0300

    add database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// Add your code here
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors({}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send({ working: true });
+});
+
+app.listen("4000");
+
+export { app };
