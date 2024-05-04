21853dbd8b992da74f1703ffeec3c69ce7dbee2a
julio cesar <jcmcf2012@gmail.com>
2024-05-04 08:55:43 -0300

    mudar sequelize

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-"use strict";
-const express = require("express");
-const cors = require("cors");
-const { connectDatabase } = require("./database/config");
-
-// Create an Express application
+import { sequelize } from "./database/connect.js";
+import router from "./User/route.js";
+import express from "express";
+import cors from "cors";
 const app = express();
-
-// Use CORS and JSON body parsing middleware
-app.use(cors());
+app.use(cors({}));
 app.use(express.json());
 
-// Define a simple GET endpoint
-app.get("/", (req, res) => {
-  res.send({ working: true });
-});
-
 (async () => {
-  await connectDatabase();
-  app.listen(4000, () => {
-    console.log("Server is running on port 4000");
-  });
+  try {
+    console.log("ENTROU");
+    await sequelize.authenticate();
+    console.log("DB AUTH");
+  } catch (error) {
+    console.log("ERROR", error);
+  }
 })();
 
-// Export the `app` instance for testing or other purposes
-module.exports = { app };
+app.use("/users", router);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
